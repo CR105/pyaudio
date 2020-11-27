2add67605d8c9f010799167f20e7c082efb5c6f3
Carlos R. Ríos <crioss83@gmail.com>
2020-11-26 22:24:38 -0600

    add new note

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import numpy as np
 import simpleaudio as sa
 #cancelador de ruido
 
-frequency = 440  # Our played note will be 440 Hz
+frequency1 = 466  # Our played note will be 440 Hz
+frequency2 = 440  # Our played note will be 440 Hz
 fs = 44100  # 44100 samples per second
-seconds = 10  # Note duration of 3 seconds
+seconds = 2  # Note duration of 3 seconds
 
 # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
 t = np.linspace(0, seconds, seconds * fs, False)
 
 # Generate a 440 Hz sine wave
-note = np.sin(frequency * t * 2 * np.pi)
+note1 = np.sin(frequency1 * t * 2 * np.pi)
+note2 = np.sin(frequency2 * t * 2 * np.pi)
 
 # Ensure that highest value is in 16-bit range
-audio = note * (2**15 - 1) / np.max(np.abs(note))
+audio = (note1 + note2) * (2**15 - 1) / np.max(np.abs(note1))
 # Convert to 16-bit data
 audio = audio.astype(np.int16)
 
